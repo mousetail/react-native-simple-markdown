9a026a1fdbc4aa693d88099e4bcccbf9822f8d34
Bastiaan <b.m.mevius@student.tue.nl>
2019-02-14 12:09:00 +0100

    editor can make html things bold

--- src/Editor.js
+++ src/Editor.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import {Button, WebView, Keyboard, Text, View} from 'react-native';
+import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource'
 
 export class RichTextEditor extends React.Component {
     constructor(props) {
@@ -7,62 +8,57 @@ export class RichTextEditor extends React.Component {
         this.webView = React.createRef();
         this.state = {
             value: "",
+            html: undefined,
         };
     }
 
+    componentDidMount(): void {
+        const path = resolveAssetSource(require('./html/communication.html'));
+        let source;
 
+        if (__DEV__) {
+            source = {uri: path.uri};
+        } else if (Platform.OS === 'ios') {
+            source = {uri: 'file://' + source.uri};
+        } else {
+            source = {uri: 'file:///android_asset/html/communication.html'};
+        }
+
+        fetch(source.uri).then(
+            (e) => e.text()
+        ).then(
+            (html) => this.setState({html: html})
+        ).catch(
+            (err) => console.error(err)
+        )
+    }
 
     render() {
-        let jsCode = `
-            document.querySelector("#test").addEventListener('input', function() {
-                window.postMessage("test");
-                try {
-                    var selection = document.getSelection();
-                    var range = selection.getRangeAt(0);
-                    
-                    window.postMessage("range: "+range.endOffset);
-                }
-                catch (err) {
-                    window.postMessage("error"+err);
-                }
-            });
-            document.querySelector('*').style.backgroundColor = 'blue';
-            document.addEventListener('message', function(e) {
-                document.getElementById('test').innerHTML = "<b>" + document.getElementById('test').innerHTML + "</b>";
-            });
-        `;
-
         return (
             <Fragment>
-                <Button
-                    title={'Make bold'}
-                    onPress={() => this.webView.current.postMessage('bold')}
-                />
+                {this.state.html &&
                 <WebView
                     ref={this.webView}
                     onMessage={ev => {
                         console.log(ev.nativeEvent.data);
                     }}
-                    source={{
-                        html: '' +
-                            '<div ' +
-                            'id="test" contenteditable="true" ' +
-                            'oninput="sendSelection()"' +
-                            '>' +
-                            'Test text' +
-                            '</div>'
-                    }}
+                    source={{html: this.state.html}}
                     javaScriptEnabled={true}
                     style={{
                         backgroundColor: '#FFFFFF'
                     }}
                     onError={ev => console.log(ev)}
-                    injectedJavaScript={
-                        jsCode
-                    }
+                    domStorageEnabled={true}
                 >
                     <Text>Within Editor Component</Text>
-                </WebView>
+                </WebView>}
+                <Button
+                    title={'Make bold'}
+                    style={{
+
+                    }}
+                    onPress={() => this.webView.current.postMessage(JSON.stringify({command: 'bold'}))}
+                />
             </Fragment>
         );
     }
